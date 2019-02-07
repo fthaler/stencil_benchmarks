a8747e8f1059b0d210aa817ff502038131ac6ffe
Felix Thaler <thaler@cscs.ch>
2019-02-07 13:42:55 +0100

    Cleanup

--- src/platform/knl/allocator.h
+++ src/platform/knl/allocator.h
@@ -20,16 +20,16 @@ namespace platform {
 
             value_type *allocate(std::size_t n) const {
                 static std::size_t offset = 64;
-                char *raw_ptr = reinterpret_cast<char *>(mmap(nullptr,
+                void *raw_ptr = mmap(nullptr,
                     n * sizeof(value_type) + offset,
                     PROT_READ | PROT_WRITE,
                     MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE | MAP_HUGETLB,
                     -1,
-                    0));
-                if (raw_ptr == reinterpret_cast<char *>(-1))
+                    0);
+                if (raw_ptr == MAP_FAILED)
                     throw ERROR("could not allocate memory");
 
-                value_type *ptr = reinterpret_cast<value_type *>(raw_ptr + offset);
+                value_type *ptr = reinterpret_cast<value_type *>(reinterpret_cast<char *>(raw_ptr) + offset);
 
                 std::size_t *offset_ptr = reinterpret_cast<std::size_t *>(ptr) - 1;
                 *offset_ptr = offset;
@@ -43,7 +43,7 @@ namespace platform {
                 std::size_t *offset_ptr = reinterpret_cast<std::size_t *>(ptr) - 1;
                 std::size_t offset = *offset_ptr;
 
-                char *raw_ptr = reinterpret_cast<char *>(ptr) - offset;
+                void *raw_ptr = reinterpret_cast<void *>(reinterpret_cast<char *>(ptr) - offset);
                 munmap(raw_ptr, n * sizeof(value_type) + offset);
             }
 
