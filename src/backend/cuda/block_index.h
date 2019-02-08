ccee3f8d95aa28cb530c876c69640b24a38d9f01
Felix Thaler <thaler@cscs.ch>
2019-02-08 14:50:00 +0100

    Added CUDA blocked_execution_2d

--- src/backend/cuda/block_index.h
+++ src/backend/cuda/block_index.h
@@ -7,7 +7,10 @@ namespace backend {
     namespace cuda {
 
         template <int IMinusHalo, int IPlusHalo = IMinusHalo, int JMinusHalo = IMinusHalo, int JPlusHalo = JMinusHalo>
-        struct block_index {
+        struct block_index_2d {
+            static_assert(
+                IMinusHalo >= 0 && IPlusHalo >= 0 && JMinusHalo >= 0 && JPlusHalo >= 0, "invalid block halo size");
+
             __device__ static constexpr int iminus_halo() { return IMinusHalo; }
             __device__ static constexpr int iplus_halo() { return IPlusHalo; }
             __device__ static constexpr int jminus_halo() { return JMinusHalo; }
@@ -18,32 +21,24 @@ namespace backend {
                 return IMinusHalo;
             }
 
-            static_assert(
-                IMinusHalo >= 0 && IPlusHalo >= 0 && JMinusHalo >= 0 && JPlusHalo >= 0, "invalid block halo size");
             int iblocksize, jblocksize, kblocksize;
             int i, j, k;
-            int iblock, jblock, kblock;
+            int iblock, jblock;
 
-            __device__ __forceinline__ block_index(int isize, int jsize, int ksize)
+            __device__ __forceinline__ block_index_2d(int isize, int jsize)
                 : iblocksize(blockDim.x),
-                  jblocksize(blockDim.y - JMinusHalo - JPlusHalo - (IMinusHalo > 0 ? 1 : 0) - (IPlusHalo > 0 ? 1 : 0)),
-                  kblocksize(blockDim.z) {
+                  jblocksize(blockDim.y - JMinusHalo - JPlusHalo - (IMinusHalo > 0 ? 1 : 0) - (IPlusHalo > 0 ? 1 : 0)) {
                 compute_block_indices();
                 const int iblockbase = blockIdx.x * iblocksize;
                 const int jblockbase = blockIdx.y * jblocksize;
-                const int kblockbase = blockIdx.z * kblocksize;
                 i = iblockbase + iblock;
                 j = jblockbase + jblock;
-                k = kblockbase + kblock;
                 if (iblockbase + iblocksize > isize)
                     iblocksize = isize - iblockbase;
                 if (jblockbase + jblocksize > jsize)
                     jblocksize = jsize - jblockbase;
-                if (kblockbase + kblocksize > ksize)
-                    kblocksize = ksize - kblockbase;
                 assert(iblocksize > 0);
                 assert(jblocksize > 0);
-                assert(kblocksize > 0);
             }
 
             __device__ __forceinline__ bool in_block_or_halo(
@@ -53,7 +48,7 @@ namespace backend {
                 assert(jminus_halo <= JMinusHalo);
                 assert(jplus_halo <= JPlusHalo);
                 return iblock >= -iminus_halo && iblock < iblocksize + iplus_halo && jblock >= -jminus_halo &&
-                       jblock < jblocksize + jplus_halo && kblock >= 0 && kblock < kblocksize;
+                       jblock < jblocksize + jplus_halo;
             }
 
             __device__ __forceinline__ bool in_block_or_halo() const {
@@ -61,12 +56,11 @@ namespace backend {
             }
 
             __device__ __forceinline__ bool in_block() const {
-                return iblock >= 0 && iblock < iblocksize && jblock >= 0 && jblock < jblocksize && kblock >= 0 &&
-                       kblock < kblocksize;
+                return iblock >= 0 && iblock < iblocksize && jblock >= 0 && jblock < jblocksize;
             }
 
             __device__ __forceinline__ int blocksize_with_halo() const {
-                return (iblocksize + IMinusHalo + IPlusHalo) * (jblocksize + JMinusHalo + JPlusHalo) * kblocksize;
+                return (iblocksize + IMinusHalo + IPlusHalo) * (jblocksize + JMinusHalo + JPlusHalo);
             }
 
           private:
@@ -74,7 +68,6 @@ namespace backend {
             __device__ __forceinline__ typename std::enable_if<AllZero>::type compute_block_indices() {
                 iblock = threadIdx.x;
                 jblock = threadIdx.y;
-                kblock = threadIdx.z;
             }
 
             template <bool AllZero = (IMinusHalo == 0 && IPlusHalo == 0 && JMinusHalo == 0 && JPlusHalo == 0)>
@@ -97,11 +90,44 @@ namespace backend {
                     iblock = int(threadIdx.x) % pad + iblocksize;
                     jblock = int(threadIdx.x) / pad - JMinusHalo;
                 }
-                kblock = threadIdx.z;
             }
 
             __device__ __forceinline__ static constexpr int block_halo_padding(int x) {
                 return x == 0 ? 0 : x == 1 ? 1 : 2 * block_halo_padding((x + 1) / 2);
+            }
+        };
+
+        template <int IMinusHalo, int IPlusHalo = IMinusHalo, int JMinusHalo = IMinusHalo, int JPlusHalo = JMinusHalo>
+        struct block_index : block_index_2d<IMinusHalo, IPlusHalo, JMinusHalo, JPlusHalo> {
+            using base_t = block_index_2d<IMinusHalo, IPlusHalo, JMinusHalo, JPlusHalo>;
+
+            int kblocksize;
+            int k;
+            int kblock;
+
+            __device__ __forceinline__ block_index(int isize, int jsize, int ksize)
+                : base_t(isize, jsize), kblocksize(blockDim.z), kblock(threadIdx.z) {
+                const int kblockbase = blockIdx.z * kblocksize;
+                k = kblockbase + kblock;
+                if (kblockbase + kblocksize > ksize)
+                    kblocksize = ksize - kblockbase;
+                assert(kblocksize > 0);
+            }
+
+            __device__ __forceinline__ bool in_block_or_halo(
+                int iminus_halo, int iplus_halo, int jminus_halo, int jplus_halo) const {
+                return base_t::in_block_or_halo(iminus_halo, iplus_halo, jminus_halo, jplus_halo) &&
+                       kblock < kblocksize;
+            }
+
+            __device__ __forceinline__ bool in_block_or_halo() const {
+                return in_block_or_halo(IMinusHalo, IPlusHalo, JMinusHalo, JPlusHalo);
+            }
+
+            __device__ __forceinline__ bool in_block() const { return base_t::in_block() && kblock < kblocksize; }
+
+            __device__ __forceinline__ int blocksize_with_halo() const {
+                return base_t::blocksize_with_halo() * kblocksize;
             }
         };
 
--- src/backend/cuda/blocked_execution.h
+++ src/backend/cuda/blocked_execution.h
@@ -7,9 +7,9 @@ namespace backend {
     namespace cuda {
 
         template <int IMinusHalo, int IPlusHalo = IMinusHalo, int JMinusHalo = IMinusHalo, int JPlusHalo = JMinusHalo>
-        class blocked_execution {
+        class blocked_execution_2d {
           public:
-            using block_index_t = block_index<IMinusHalo, IPlusHalo, JMinusHalo, JPlusHalo>;
+            using block_index_t = block_index_2d<IMinusHalo, IPlusHalo, JMinusHalo, JPlusHalo>;
 
             static constexpr int iminus_halo() { return IMinusHalo; }
             static constexpr int iplus_halo() { return IPlusHalo; }
@@ -23,47 +23,74 @@ namespace backend {
 
             static void register_arguments(arguments &args) {
                 args.add({"i-blocksize", "block size in i-direction", "32"})
-                    .add({"j-blocksize", "block size in j-direction", "8"})
-                    .add({"k-blocksize", "block size in k-direction", "1"});
+                    .add({"j-blocksize", "block size in j-direction", "8"});
             }
 
-            blocked_execution(const arguments_map &args)
-                : m_isize(args.get<int>("i-size")), m_jsize(args.get<int>("j-size")), m_ksize(args.get<int>("k-size")),
-                  m_iblocksize(args.get<int>("i-blocksize")), m_jblocksize(args.get<int>("j-blocksize")),
-                  m_kblocksize(args.get<int>("k-blocksize")) {
+            blocked_execution_2d(const arguments_map &args)
+                : m_isize(args.get<int>("i-size")), m_jsize(args.get<int>("j-size")),
+                  m_iblocksize(args.get<int>("i-blocksize")), m_jblocksize(args.get<int>("j-blocksize")) {
                 if (m_iblocksize <= 0)
                     throw ERROR("invalid i-blocksize");
                 if (m_jblocksize <= 0)
                     throw ERROR("invalid j-blocksize");
-                if (m_kblocksize <= 0)
-                    throw ERROR("invalid k-blocksize");
                 constexpr int max_i_halo = IMinusHalo > IPlusHalo ? IMinusHalo : IPlusHalo;
                 if ((m_jblocksize + JMinusHalo + JPlusHalo) * max_i_halo > std::min(m_iblocksize, 32)) {
                     throw ERROR("unsupported block size");
                 }
             }
 
-            dim3 blocks() const {
-                return dim3(rndup_div(m_isize, m_iblocksize),
-                    rndup_div(m_jsize, m_jblocksize),
-                    rndup_div(m_ksize, m_kblocksize));
-            }
+            dim3 blocks() const { return dim3(rndup_div(m_isize, m_iblocksize), rndup_div(m_jsize, m_jblocksize), 1); }
 
             dim3 threads() const {
                 return dim3(m_iblocksize,
                     m_jblocksize + JMinusHalo + JPlusHalo + (IMinusHalo > 0 ? 1 : 0) + (IPlusHalo > 0 ? 1 : 0),
-                    m_kblocksize);
+                    1);
             }
 
             int blocksize_with_halo() const {
-                return (m_iblocksize + IMinusHalo + IPlusHalo) * (m_jblocksize + JMinusHalo + JPlusHalo) * m_kblocksize;
+                return (m_iblocksize + IMinusHalo + IPlusHalo) * (m_jblocksize + JMinusHalo + JPlusHalo);
             }
 
-          private:
+          protected:
             static constexpr int rndup_div(int den, int div) { return (den + div - 1) / div; }
 
-            int m_isize, m_jsize, m_ksize;
-            int m_iblocksize, m_jblocksize, m_kblocksize;
+            int m_isize, m_jsize;
+            int m_iblocksize, m_jblocksize;
+        };
+
+        template <int IMinusHalo, int IPlusHalo = IMinusHalo, int JMinusHalo = IMinusHalo, int JPlusHalo = JMinusHalo>
+        class blocked_execution : blocked_execution_2d<IMinusHalo, IPlusHalo, JMinusHalo, JPlusHalo> {
+            using base_t = blocked_execution_2d<IMinusHalo, IPlusHalo, JMinusHalo, JPlusHalo>;
+
+          public:
+            using block_index_t = block_index<IMinusHalo, IPlusHalo, JMinusHalo, JPlusHalo>;
+
+            static void register_arguments(arguments &args) {
+                base_t::register_arguments(args);
+                args.add({"k-blocksize", "block size in k-direction", "1"});
+            }
+
+            blocked_execution(const arguments_map &args)
+                : base_t(args), m_ksize(args.get<int>("k-size")), m_kblocksize(args.get<int>("k-blocksize")) {
+                if (m_kblocksize <= 0)
+                    throw ERROR("invalid k-blocksize");
+            }
+
+            dim3 blocks() const {
+                dim3 blocks_2d = base_t::blocks();
+                return dim3(blocks_2d.x, blocks_2d.y, base_t::rndup_div(m_ksize, m_kblocksize));
+            }
+
+            dim3 threads() const {
+                dim3 threads_2d = base_t::threads();
+                return dim3(threads_2d.x, threads_2d.y, m_kblocksize);
+            }
+
+            int blocksize_with_halo() const { return base_t::blocksize_with_halo() * m_kblocksize; }
+
+          protected:
+            int m_ksize;
+            int m_kblocksize;
         };
     } // namespace cuda
 } // namespace backend
